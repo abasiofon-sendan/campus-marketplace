e4a31b432b4f5012ea268f5ff28f97461041936e
Covenant Monday <covenantmonday863@gmail.com>
2025-11-26 16:33:45 +0100

    created models and serializers

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class TopCustomers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="top_customer")
+    total_purchases = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.total_purchases}"
+    
+
+class TopVendors(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="top_vendor")
+    total_sales = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.total_sales}"
--- backend/customers/serializers.py
+++ backend/customers/serializers.py
@@ -1,1 +1,14 @@
+from rest_framework import serializers
+from .models import TopCustomers, TopVendors
 
+
+class TopCustomersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TopCustomers
+        fields = ['user', 'total_purchases']
+
+
+class TopVendorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TopVendors
+        fields = ['user', 'total_sales']
