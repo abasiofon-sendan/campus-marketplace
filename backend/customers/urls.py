ea9d301c09298d4db611291319edde6ffb3222bd
Covenant Monday <covenantmonday863@gmail.com>
2025-12-03 14:49:51 +0100

    urls

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import TopCustomersView, TopVendorsView
+from .views import GetUserProfileAndContents, TopCustomersView, TopVendorsView, UpdateProfileView, EditProfileView, DeleteProfileView, UploadContentView, UserDeleteProfile
 
 
 urlpatterns = [
     path('top-customers/', TopCustomersView.as_view(), name='top-customers'),
     path('top-vendors/', TopVendorsView.as_view(), name='top-vendors'),
+    path('profile/update/', UpdateProfileView.as_view(), name='update-profile'),
+    path('profile/edit/<int:pk>/', EditProfileView.as_view(), name='edit-profile'),
+    path('profile/delete/<int:pk>/', DeleteProfileView.as_view(), name='delete-profile'),
+    path('user/delete/', UserDeleteProfile.as_view(), name='user-delete-profile'),
+    path('profile/<int:pk>/', GetUserProfileAndContents.as_view(), name='get-profile'),
+    path('content/upload/', UploadContentView.as_view(), name='upload-content'),
+
 ]
