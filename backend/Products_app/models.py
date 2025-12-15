5df103a0a167c90e219b0122f7178b6e925af843
battleangel217 <idaraobong05@gmail.com>
2025-12-15 09:29:24 +0100

    added rating, switched to vendorid, add review

--- backend/Products_app/models.py
+++ backend/Products_app/models.py
@@ -3,16 +3,26 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 # Create your models here.
+
 class Product(models.Model):
-    vendor_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
+    vendor_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
     product_name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     quantity = models.PositiveIntegerField()
     category = models.CharField(max_length=255)
     image_url= models.JSONField(max_length=255,blank=True,null=True, default=list)
-    rating = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField(default=0)
     view_count = models.PositiveIntegerField(default=0, null=True,blank=True)
     # rating = models.ForeignKey()
     def __str__(self):
         return f"{self.product_name} ({self.vendor_name.username})"
+    
+
+
+class ProductReviews(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="customer")
+    rating = models.PositiveIntegerField()
+    review = models.TextField()
+
