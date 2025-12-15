1e558c75ae94b2364fbd82ffc91e688c41e27ac5
battleangel217 <idaraobong05@gmail.com>
2025-12-15 09:39:51 +0100

    Updated the url to fit the get cart view

--- backend/userCart/urls.py
+++ backend/userCart/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CarItemView
+from .views import CarItemView, GetCartItemView
 
 urlpatterns = [
-    path('cart-items/', CarItemView.as_view(), name='cart-items'),
+    path('cart-items/', GetCartItemView.as_view(), name='cart-items'),
     # path('cart-items/add', CarItemView.as_view(), name='cart-item-add'),
     # delete a specific cart item by its PK: DELETE /cart/cart-items/<pk>/
-    path('cart-items/<int:pk>/', CarItemView.as_view(), name='cart-item-detail'),
+    path('cart-items/<int:pk>', CarItemView.as_view(), name='cart-item-detail'),
 ]
