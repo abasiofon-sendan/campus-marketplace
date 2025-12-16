6b161309bc9a06549e4069936b3a157664de41ed
battleangel217 <idaraobong05@gmail.com>
2025-12-16 11:07:56 +0100

    Added image url to Top selling products

--- backend/analytics/serializers.py
+++ backend/analytics/serializers.py
@@ -16,7 +16,8 @@ class ProductStockSnapshotSerializer(serializers.ModelSerializer):
 class TopSellingProductSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.product_name', read_only=True)
     product_price = serializers.DecimalField(source='product.price', read_only=True, max_digits=10, decimal_places=2)
+    image_url = serializers.JSONField(source="product.image_url", read_only=True)
     
     class Meta:
         model = TopSellingProduct
-        fields = ['id', 'product', 'product_name', 'product_price', 'units_sold', 'revenue_generated', 'period_start', 'period_end', 'rank']
+        fields = ['id', 'product', 'product_name', 'product_price', 'units_sold', 'revenue_generated', 'period_start', 'period_end', 'rank', 'image_url']
