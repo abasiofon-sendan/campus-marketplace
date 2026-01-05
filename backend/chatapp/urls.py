78beeb6847a8464fb73728aab46c9aca4738afa5
battleangel217 <idaraobong05@gmail.com>
2026-01-05 15:21:28 +0100

    redis url

--- backend/chatapp/urls.py
+++ backend/chatapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GetAllConversationsView, CreateConversationView
+from .views import CreateConversationView
 
 urlpatterns = [
     path('create/<int:pk>', CreateConversationView.as_view(), name="Create conversations")
