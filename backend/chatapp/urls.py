b2ad6ad21310270a53ee9aeb98213fab5abbb73b
battleangel217 <idaraobong05@gmail.com>
2026-01-05 14:23:06 +0100

    deployment error

--- backend/chatapp/urls.py
+++ backend/chatapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import GetAllConversationsView, CreateConversationView
 
 urlpatterns = [
-    path('mychats/', GetAllConversationsView.as_view(), name='my chats'),
     path('create/<int:pk>', CreateConversationView.as_view(), name="Create conversations")
 ]
