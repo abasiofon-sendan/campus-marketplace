0a0129d4aed4f2417311d65e185e826b97c4934c
battleangel217 <idaraobong05@gmail.com>
2026-01-12 14:36:23 +0100

    Incomplete: Order

---
+++ backend/orders/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
+from Products_app.models import Product
+
+User = get_user_model()
+
+# Create your models here.
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted by vendor'),
+        ('awaiting', 'Waiting for delivery'),
+        ('completed', 'Completed'),
+        ('expired', 'Expired - Refunded')
+    ]
+
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchases')
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sales")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='pending')
+
+    qr_code = models.UUIDField(default=uuid.uuid4, unique=True)
+    qr_expires_at = models.DateTimeField(blank=True, null=True)
+    qr_scanned = models.BooleanField(default=False)
+    qr_scanned_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
---
+++ backend/orders/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Order
+from .serializers import OrderSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
+
+# Create your views here.
+
+
+class GetAllOrders(APIView):
+    def get(self, request):
+        user = request.user
+        data = Order.objects.filter(
+            Q(vendor=user) | Q(buyer=user)
+        ).select_related("vendor", "buyer")
+
