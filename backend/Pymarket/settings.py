f4198d478a7719ca1463fd9a0a673399f6622be7
battleangel217 <idaraobong05@gmail.com>
2026-01-13 06:20:41 +0100

    Incomplete: Order

--- backend/Pymarket/settings.py
+++ backend/Pymarket/settings.py
@@ -117,15 +117,15 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default': dj_database_url.config(
-    #         default=os.environ.get('DATABASE_URL'),
-    #         conn_max_age=60,  # Shorter for free tier
-    #         conn_health_checks=True,  # Automatically reconnect if connection dies
-    #     )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=60,  # Shorter for free tier
+            conn_health_checks=True,  # Automatically reconnect if connection dies
+        )
     }
 
 
