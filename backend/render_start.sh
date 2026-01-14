72573588fdf0ddbfff3553490b85e83688b6b5da
battleangel217 <idaraobong05@gmail.com>
2026-01-14 18:45:02 +0100

    removed statiuc

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -4,8 +4,5 @@
 # Run migrations
 python manage.py migrate --noinput
 
-# Collect static files
-python manage.py collectstatic --noinput
-
 # Start Daphne ASGI server (required for WebSocket support)
 daphne -b 0.0.0.0 -p $PORT Pymarket.asgi:application
